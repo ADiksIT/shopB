7be92698f683e5bf4f465f2b989c58248477d743
ADiks <antondikson@gmail.com>
2020-04-16 13:22:47 +0300

    finanSinglePAge

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// 'use strict';
+// const browseBtn = document.querySelector('.browse'),
+// dropdowm = document.querySelector('.dropdown');
+
+// browseBtn.addEventListener('zz', () => {
+//     dropdowm.style.display = 'block';
+// });
+// browseBtn.addEventListener('mouseout', () => {
+//     dropdowm.style.display = 'none';
+// });
